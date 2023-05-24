e4997ef65db5233618c7a01ceea4976ec462c285
loretoparisi <loretoparisi@gmail.com>
2023-05-24 15:57:36 +0200

    FPS throttling

--- flappyBird.js
+++ flappyBird.js
@@ -12,6 +12,44 @@ function rotateAndPaintImage(context, image, angleInRad, positionX, positionY, a
     context.rotate(-angleInRad);
     context.translate(-positionX, -positionY);
 }
+///// FPS ////
+// the animation loop calculates time elapsed since the last loop
+// and only draws if your specified fps interval is achieved
+function animate(draw) {
+
+    // request another frame
+
+    requestAnimationFrame(function() {
+        animate(draw);
+    });
+
+    // calc elapsed time since last loop
+
+    now = Date.now();
+    elapsed = now - then;
+
+    // if enough time has elapsed, draw the next frame
+
+    if (elapsed > fpsInterval) {
+
+        // Get ready for next frame by setting then=now, but also adjust for your
+        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+        then = now - (elapsed % fpsInterval);
+
+        // Put your drawing code here
+        if(draw) draw();
+    }
+}
+// initialize the timer variables and start the animation
+var stop = false;
+var frameCount = 0;
+var fps, fpsInterval, startTime, now, then, elapsed;
+function startAnimating(fps, draw) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    animate(draw);
+}
 ///// TRUCCHI DEL MESTIERE (HACKING) ////////////////////
 Audio.prototype.play = (function (play) {
     return function () {
@@ -46,7 +84,7 @@ const SCENARI = [
 ];
 
 // SPRITE
-const SPRITES = [
+var SPRITES = [
     "megaman.gif",
     "bird.png",
     "lama.gif",
@@ -55,6 +93,12 @@ const SPRITES = [
     "pikachu_3s.gif",
     "jessica.gif",
     "marios.gif"
+];
+
+SPRITES = [
+    "pikachu_1s.gif",
+    "pikachu_2s.gif",
+    "pikachu_3s.gif"
 ];
 
 // IMMAGINI SPRITE E SCENARI
@@ -85,7 +129,7 @@ pipeSouth.src = "images/pipeSouth.png";
 
 
 // VARIABILI DEL GIOCO
-var gap = 85;
+var gap = 125;
 var constant;
 
 var bX = 10;
@@ -100,6 +144,8 @@ var score = 0;
 // MUSICA ED EFFETTI SONORI
 var fly = new Audio();
 var scor = new Audio();
+var scor5 = new Audio();
+var scor10 = new Audio();
 var boom = new Audio();
 // COLONNA SONORA (soundtrack)
 var soundtrack = new Audio();
@@ -108,6 +154,8 @@ soundtrack.volume = 0.2;
 
 fly.src = "sounds/fly.mp3";
 scor.src = "sounds/score.mp3";
+scor5.src = "sounds/score5.wav";
+scor10.src = "sounds/score10.wav";
 boom.src = "sounds/win.mp3";
 soundtrack.src = "sounds/Eternity.mp3";
 
@@ -184,9 +232,15 @@ function DISEGNA() {
             }
         }
 
-        if (pipe[i].x == 5) {
+        if (pipe[i].x == 5) { // score
             score++;
-            scor.play();
+            if(score % 10 == 0) { // every ten
+                scor10.play();
+            } else if(score % 5 == 0) { // every five
+                scor5.play();
+            } else {
+                scor.play();
+            }
         }
     }
     bY += gravity;
@@ -195,7 +249,8 @@ function DISEGNA() {
     ctx.font = "20px Verdana";
     ctx.fillText("Punti: " + score, 10, cvs.height - 20);
 
-    requestAnimationFrame( DISEGNA );
+    //requestAnimationFrame( DISEGNA );
+    startAnimating(100, DISEGNA )
 
 }
 DISEGNA();
